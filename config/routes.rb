5ae7371413a58109c19f225001d3166ba158ccfa
Zaid Soboh <zaidsoboh@hotmail.com>
2020-05-11 20:47:23 +1000

    signup page is now the root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  root 'application#hello'
+  # get 'users/new'
+  # root 'application#hello'
+  root 'users#new'
+  get    '/signup'    => 'users#new'
 end
