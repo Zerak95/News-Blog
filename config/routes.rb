6622f3e290192990ffa4ff66c9870b1095693112
Zaid Soboh <zaidsoboh@hotmail.com>
2020-05-13 22:50:55 +1000

    added login and logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,11 @@ Rails.application.routes.draw do
   # root 'application#hello'
   # post '/signup',  to: 'users#create'
 
-  root 'users#new'
-  get  '/signup' => 'users#new'
-  post '/signup' => 'users#create'
+  root   'users#new'
+  get    '/signup' => 'users#new'
+  post   '/signup' => 'users#create'
+  get    '/login'  => 'sessions#new'
+  post   '/login'  => 'sessions#create'
+  delete '/logout' => 'sessions#destroy'
   resources :users
 end
