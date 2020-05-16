32b020d5e0401646c4b795b41c6226c4ca5f2117
Zaid Soboh <zaidsoboh@hotmail.com>
2020-05-17 01:40:09 +1000

    updated the user signup test to include phone number

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -21,6 +21,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_difference 'User.count', 1 do
       post users_path, params: { user: { name:  "Example User",
                                          email: "user@example.com",
+                                         phone_number: "1234567890",
                                          password:              "password",
                                          password_confirmation: "password" } }
     end
