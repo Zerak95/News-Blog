b764841bcdb8ec731333a74686e0b0cce36d4612
Zaid Soboh <zaidsoboh@hotmail.com>
2020-05-25 22:59:54 +1000

    new Topics now start with a defult view_count of 0

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_25_002712) do
+ActiveRecord::Schema.define(version: 2020_05_25_125057) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "topics", force: :cascade do |t|
     t.string "name"
-    t.integer "view_count"
+    t.integer "view_count", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
