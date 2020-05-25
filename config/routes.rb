0b5284bba68b9332fa0a830764f76b4a8fbffd64
Zaid Soboh <zaidsoboh@hotmail.com>
2020-05-26 06:59:42 +1000

    added new routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
+  # get 'sessions/new'
   # get 'users/new'
   # root 'application#hello'
   # post '/signup',  to: 'users#create'
 
-  root   'sessions#home'
+  root   'posts#recent'
   get    '/signup' => 'users#new'
   post   '/signup' => 'users#create'
   get    '/login'  => 'sessions#new'
   post   '/login'  => 'sessions#create'
   delete '/logout' => 'sessions#destroy'
   resources :users
+  resources :posts, only: [:new, :create, :destroy]
 end
