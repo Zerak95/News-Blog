1f5d2307c94eed6a7d9a6730452c32a2d239230f
Zaid Soboh <zaidsoboh@hotmail.com>
2020-05-25 23:48:55 +1000

    added more validation to the Post model to pass the validation test

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :user
   belongs_to :topic
+  validates :user_id, presence: true
+  validates :topic_id, presence: true
+  validates :title, presence: true, length: { minimum: 5, maximum: 50 }
+  validates :content, presence: true, length: { maximum: 1000 }
 end
