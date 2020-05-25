6148a82a03a67f21f87e77650b39f7ab7a010100
Zaid Soboh <zaidsoboh@hotmail.com>
2020-05-25 23:48:04 +1000

    added Topic validation tests

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,51 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:zerak)
+    @topic = topics(:one)
+    
+    # This code is not idiomatically correct.
+    @post = Post.new(content: "Lorem ipsum", user_id: @user.id,
+                       topic_id: @topic.id, title: "Lorem ipsum")
+  end
+
+  test "should be valid" do
+    assert @post.valid?
+  end
+
+  test "title should be present" do
+    @post.title = "     "
+    assert_not @post.valid?
+  end
+
+  test "title should not be too long" do
+    @post.title = "a" * 51
+    assert_not @post.valid?
+  end
+
+  test "title should not be too short" do
+    @post.title = "a" * 4
+    assert_not @post.valid?
+  end
+
+  test "user id should be present" do
+    @post.user_id = nil
+    assert_not @post.valid?
+  end
+
+  test "topic id should be present" do
+    @post.topic_id = nil
+    assert_not @post.valid?
+  end
+
+  test "content should be present" do
+    @post.content = "   "
+    assert_not @post.valid?
+  end
+
+  test "content should be at most 1000 characters" do
+    @post.content = "a" * 1001
+    assert_not @post.valid?
+  end
 end
