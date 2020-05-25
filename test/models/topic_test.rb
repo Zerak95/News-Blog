c50db071ac560bbcefee6929fad58bfd3fada854
Zaid Soboh <zaidsoboh@hotmail.com>
2020-05-25 22:58:16 +1000

    avlidation tests for the Topic model

--- test/models/topic_test.rb
+++ test/models/topic_test.rb
@@ -1,8 +1,32 @@
 require 'test_helper'
 
 class TopicTest < ActiveSupport::TestCase
-  test "Topic should be valid" do
+
+  def setup
     @topic = Topic.new(name: "test topic")
+  end
+
+  test "Topic should be valid" do
     assert @topic.valid?
+  end
+
+  test "name should be present" do
+    @topic.name = "     "
+    assert_not @topic.valid?
+  end
+
+  test "name should be unique" do
+    @topic.save
+    @topic2 = Topic.new(name: "test topic")
+    assert_not @topic2.valid?
+  end
+
+  test "name should not be too long" do
+    @topic.name = "a" * 26
+    assert_not @topic.valid?
+  end
+
+  test "view_count should start at 0" do
+    assert @topic.view_count == 0
   end
 end
