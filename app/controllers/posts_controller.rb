e1187fa4f95f5b1bb7d4ed2134d1ba0191648422
Zaid Soboh <zaidsoboh@hotmail.com>
2020-05-26 10:30:20 +1000

    tests for pots url and the path implementaion done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,9 +4,38 @@ class PostsController < ApplicationController
         # beginning_of_month = Date.today.beginning_of_month
         # end_of_month = beginning_of_month.end_of_month
         # @posts = Post.where(created_at: beginning_of_month..end_of_month)
+        @posts = Post.all
     end
 
     def new
-      @post = Post.new
+      if logged_in?
+        @post = Post.new
+      else
+        redirect_to login_path
+      end
+    end
+
+    def create
+        if logged_in?
+
+            @user = current_user
+            @topic_id = params.require(:post).permit(:topic_id)
+            if @topic_id.nil?
+                @post = @user.posts.build( params.require(:post).permit(:content,
+                                                                        :title))
+            else
+                @post = @user.posts.build( params.require(:post).permit(:topic_id,
+                                                            :content, :title))
+            end
+
+            if @post.save
+                flash[:success] = "A new post has been created"
+                redirect_to root_url
+            else
+                render 'new'
+            end
+        else
+            redirect_to login_path
+        end
     end
 end
--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -2,14 +2,23 @@ require 'test_helper'
 
 class PostsControllerTest < ActionDispatch::IntegrationTest
   def setup
-    # @user = users(:zerak)
-    # @topic = topics(:one)
+    @user = users(:zerak)
+    @topic = topics(:one)
     
-    # @post = post(:orange)
+    @post = @user.posts.build(content: "Lorem ipsum", topic_id: @topic.id,
+                                       title: "Lorem ipsum")
   end
 
-  test "should get new" do
+  test "should redirect new when not logged in" do
     get new_post_url
-    assert_response :success
+    assert_redirected_to login_url
+  end
+  
+  test "should redirect create when not logged in" do
+    assert_no_difference 'Post.count' do
+      post posts_path, params: { post: { content: "Lorem ipsum", topic_id: @topic.id,
+                                    title: "Lorem ipsum", user_id: @user.id} }
+    end
+    assert_redirected_to login_url
   end
 end
