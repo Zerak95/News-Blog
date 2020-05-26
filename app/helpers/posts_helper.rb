3763d61b678957a7f7e579c77c70ca9165825a02
Zaid Soboh <zaidsoboh@hotmail.com>
2020-05-26 10:31:18 +1000

    home page more dynamic

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,11 +1,17 @@
 module PostsHelper
 
     def post_topics
-        @topics = [["====Select====", nil]]
+        @topics = [["====Select====", no_post_topic]]
         @topics_all = Topic.all
-        @topics_all.each do|user|
-            @topics.push([user.name, user.id])
+        @topics_all.each do|topic|
+          if topic.name != "News"  
+            @topics.push([topic.name, topic.id])
+          end
         end
         return @topics
+    end
+
+    def no_post_topic
+        @topic = Topic.where(name:"News").first.id
     end
 end
