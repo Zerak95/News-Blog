06acce0c439d78cfade1b573b8553b5df01b007b
Zaid Soboh <zaidsoboh@hotmail.com>
2020-05-29 19:30:39 +1000

    seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+news = Topic.create(name:"News", view_count: 0)
+
+user = User.create(name:"test user", email: "ztest@s.com", 
+                password_digest: User.digest('password'), 
+                phone_number: "1234567890" )
