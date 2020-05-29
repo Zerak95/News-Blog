60491e5838dcc4817aa39b16990ac30a086ddd73
Zaid Soboh <zaidsoboh@hotmail.com>
2020-05-30 07:29:39 +1000

    Seed Topics

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,10 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-news = Topic.create(name:"News", view_count: 0)
-
-user = User.create(name:"test user", email: "ztest@s.com", 
-                password_digest: User.digest('password'), 
-                phone_number: "1234567890" )
+Topic.create(name:"News", view_count: 0)
+Topic.create(name:"A.I.", view_count: 0)
+Topic.create(name:"RMIT", view_count: 0)
+Topic.create(name:"AWS", view_count: 0)
+Topic.create(name:"Rails", view_count: 0)
+Topic.create(name:"Game", view_count: 0)
+Topic.create(name:"NodeJS", view_count: 0)
