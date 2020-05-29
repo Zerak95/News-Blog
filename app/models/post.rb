5d7e85d74771026eb9e3a9650bbf1f2e6a7b0ca6
Zaid Soboh <zaidsoboh@hotmail.com>
2020-05-29 13:07:08 +1000

    bug with time diplaying the wrong value

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,7 @@ class Post < ApplicationRecord
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
   validates :topic_id, presence: true
-  validates :title, presence: true, length: { minimum: 5, maximum: 50 }
-  validates :content, presence: true, length: { maximum: 1000 }
+  # validates :title, presence: true, length: { minimum: 5, maximum: 50 }
+  validates :title, presence: true, length: { minimum: 5, maximum: 100 }
+  validates :content, presence: true, length: { maximum: 2000 }
 end
