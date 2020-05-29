78622150bd53f7390143a595fe404d82764d9aed
Zaid Soboh <zaidsoboh@hotmail.com>
2020-05-30 09:23:01 +1000

    show route and page

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,5 @@ Rails.application.routes.draw do
   post   '/login'  => 'sessions#create'
   delete '/logout' => 'sessions#destroy'
   resources :users
-  resources :posts, only: [:new, :create, :destroy]
+  resources :posts, only: [:new, :create, :destroy, :show]
 end
