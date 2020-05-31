f4d1f4d26c6e2aaba125b6fc32a26a083aa663d1
Zaid Soboh <zaidsoboh@hotmail.com>
2020-05-31 16:45:56 +1000

    show first comment first

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
-  default_scope -> { order(created_at: :desc) }
+  default_scope -> { order(created_at: :asc) }
   validates :user_id, presence: true
   validates :post_id, presence: true
   validates :content, presence: true, length: { maximum: 500 }
