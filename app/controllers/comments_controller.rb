c95a90d98f13d304205a6a27b8a48f50e19e2ce3
Zaid Soboh <zaidsoboh@hotmail.com>
2020-05-31 16:36:06 +1000

    comments enabled

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,17 +4,18 @@ class CommentsController < ApplicationController
     if logged_in?
 
       @user = current_user
-      @post = Post.find(params[:id])
+      @post_id = params.require(:comment).permit(:post_id)
 
-      @comment = @user.comments.build( params.require(:comment).permit(:content),
-                                        post_id: @post.id, user_id: @user.id)
+      @post = Post.find(@post_id[:post_id])
+
+      @comment = @user.comments.build( params.require(:comment).permit(:content,:post_id))
 
       if @comment.save
         flash[:success] = "A new comment has been created"
         redirect_to @post
       else
         flash[:danger] = "The comment has not been created"
-        redirect_to @post
+        render "/posts/show"
       end
     else
         redirect_to login_path
